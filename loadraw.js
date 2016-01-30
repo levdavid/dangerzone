bb0c942ae43136a9635cc0b00be3c0323d91ec28
Greg Harris <gharris1727@gmail.com>
2016-01-30 12:14:32 -0500

    First partially working version of the data importer. Testing now.

--- loadraw.js
+++ loadraw.js
@@ -1,31 +1,47 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
+var util = require('./util.js');
 var url = 'mongodb://localhost:27017/dangerzone';
 
-var gridDigits = 5;
-var locDigits = 2;
-
-var storeIncident = function(collection, lattitude, longitude, date, name) {
-    console.log(lattitude + "," + longitude + "," + date + "," + name);
+var storeIncident = function(collection, callback, lattitude, longitude, date, name) {
+    var data = {
+        "lattitude": lattitude,
+        "longitude": longitude,
+        "date":date,
+        "name":name
+    }
+    var filter = { "cellName" : util.getCellName(lattitude, longitude) };
+    var update = { $push: { "incidents" : data }};
+    var options = { upsert: true };
+    console.log("Inserting!");
+    collection.findOneAndUpdate(filter, update, options, callback);
 }
 
-var loadFile = function(collection) {
+var loadFile = function(collection, callback) {
     var file = require('./input/moco.json');
     var data = file["data"];
     var timeIndex = 9;
     var nameIndex = 11;
     var lattitudeIndex = 24;
     var longitudeIndex = 25;
+
+    var storeCallback = function(err, res){
+        console.log("Inserted!");
+        assert.equal(err, null);
+        console.log("Success!");
+    }
+
     for (i in data) {
         var incident = data[i];
-        storeIncident(collection, incident[lattitudeIndex], incident[longitudeIndex], incident[timeIndex], incident[nameIndex]);
+        storeIncident(collection, storeCallback, 
+                incident[lattitudeIndex], incident[longitudeIndex], incident[timeIndex], incident[nameIndex]);
     }
+    callback();
 }
 
 var loadData = function(db, callback) {
-    loadFile(db.collection("data"));
-    callback();
+    loadFile(db.collection("data"), callback);
 };
 
 MongoClient.connect(url, function(err, db) {
--- util.js
+++ util.js
@@ -1,10 +1,8 @@
 var basePrecision = 3;
 var cellPrecision = 5;
 
-var getCellName = function(lattitude, longitude) {
+exports.getCellName = function(lattitude, longitude) {
     var lat = lattitude * Math.pow(10,(cellPrecision - basePrecision));
     var lon = longitude * Math.pow(10,(cellPrecision - basePrecision));
     return Math.floor(lat) + ":" + Math.floor(lon);
 }
-
-console.log(getCellName(39.188573792034447,-77.20146939275935));
