06688f3d2df285feedc3ac51d4f9f710106a8f54
lev david <levdavid1@gmail.com>
2016-01-30 11:26:22 -0500

    Lev - middleware uses

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+//base install
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/dangerzone'); // connect to our database
 
+var Bear = require('./app/models/crime'); //using this within our database
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -13,6 +17,13 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+router.use(function(req, res, next){
+	//do logging
+	console.log('something is happening');
+	next();//make sure we don't stop here
+});
+
+// test route to verify everything is 
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!'});
 });
